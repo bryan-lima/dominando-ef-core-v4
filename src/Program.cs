f5c38cad7b2623f2dd7387c11c056f4cbb2fe5f1
Bryan <bryanlima200@outlook.com>
2022-02-19 16:14:20 -0300

    Criado projeto EFCore.Tips

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCore.Tips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
